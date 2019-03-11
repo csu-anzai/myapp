1d3c53debad6fe4819a8c872561544983f819ba6
Ahmed-Shaheen <e.shaheen13@gmail.com>
2019-03-11 08:12:16 +0200

    finish home & news page

--- www/js/index.js
+++ www/js/index.js
@@ -18,19 +18,30 @@
  */
 var app = {
   home: function() {
-    // navigator.splashscreen.show();
+    console.log('show splash screen');
+    navigator.splashscreen.show();
 
 
       // get posts for slider
       const data1 = new Promise(function(resolve, reject){
-        axios.get("http://www.tantasc.net/wp-json/wp/v2/posts?_embed&categories=77&per_page=3").then(function(response) {
+        console.log('sent slider posts request');
+        // axios.get("http://www.tantasc.net/wp-json/wp/v2/posts?_embed&categories=77&per_page=5").then(function(response) {
+        axios.get("test/data.json").then(function(response) {
+          
+        console.log('recieved slider posts request');
           response.data.forEach(post => {
-            document.getElementById("slider").innerHTML += "<div class='slider'>" +
+            document.getElementById("slider").innerHTML += "<div class='slider slider1 '>" +
               "<img src='" + post._embedded['wp:featuredmedia'][0].source_url + "' alt='slider-img' class='w-100 h-100'>" +
-              "<p>" + post.title.rendered + "</p>" +
+              "<p><a href='post.html#" + post.id + "' >" + post.title.rendered + "</a></p>" +
             "</div>";
           });
         }).then(function(){
+          $('.slick').slick({
+                dots: true,
+                arrows: false,
+                rtl: true
+            });
+        console.log('finish render posts');
           resolve(true);
         });
       });
@@ -38,46 +49,82 @@ var app = {
       // get posts for latest news
 
       const data2 = new Promise(function(resolve, reject){
-        axios.get("http://www.tantasc.net/wp-json/wp/v2/posts?_embed&categories_exclude=77&per_page=3").then(function(response) {
+        console.log('sent body posts request');
+        moment().format();
+        // axios.get("http://www.tantasc.net/wp-json/wp/v2/posts?_embed&categories_exclude=77&per_page=3").then(function(response) {
+        axios.get("test/data.json").then(function(response) {
+        
+        console.log('recieved body posts request');
+
           response.data.forEach(post => {
-            document.getElementById("news-items").innerHTML += "<a href='" + post.link + "' class='media mb-4'>"+
-                      "<img class='d-flex mr-3 mb-3' src='" + post._embedded['wp:featuredmedia'][0].source_url + "' alt='صورة الخبر'>"+
-                      "<div class='media-body'>"+
-                          "<p>" + post.title.rendered + "</p>"+
-                          "<span>" + post.date + "</span>" +
-                      "</div>"+
-                  "</a>";
+
+            var date = moment(post.date);
+
+            var a = date.format('a') == 'am' ? 'صباحا' : 'مساءا';
+
+            document.getElementById("posts").innerHTML += "<a href='post.html#" + post.id + "' class='card mb-4'>" +
+              "<img class='card-img-top' src='" + post._embedded['wp:featuredmedia'][0].source_url + "' height='150px' alt='Card image cap' >" +
+              "<div class='card-body'>" +
+                "<h4 class='card-title news-title'>" + post.title.rendered + "</h4>" +
+                "<p class='card-text news-sub-title'>" +
+                "<span>" + date.locale('ar').format('dddd ') + date.locale('en').format('YYYY/M/D - h:mm ') + a + "</span><br />" +
+                "</p>" +
+                "<i class='mdi mdi-share'></i>" +
+              "</div>" +
+            "</a>";
           });
         }).then(() => {
+          console.log('finish render body posts');
           resolve(true);
         });
       });
 
       Promise.all([data1, data2]).then(function(values){
-        console.log(values);
         navigator.splashscreen.hide();
+        console.log('finish two requests and hide splash screen')
       });
     
   },
 
 
-  news: function(search) {
+  news: function(search=false) {
     var param = "";
     if(search){
-      param = "?search=" + search;
+      param = "&search=" + search;
     }
 
+    if(!sessionStorage.page){
+      sessionStorage.page = 1;
+    }
+
+    param += "&page=" + sessionStorage.page;
+
     navigator.splashscreen.show();
-      axios.get("http://www.tantasc.net/wp-json/wp/v2/posts" + param).then(function(response) {
-          response.data.forEach(post => {
-            document.getElementById("news-items").innerHTML += "<a href='" + post.link + "' class='media mb-4'>"+
-                      "<img class='d-flex mr-3 mb-3' src='img/bg/bg-3.jpg' alt='Generic placeholder image'>"+
-                      "<div class='media-body'>"+
-                          "<p>" + post.title.rendered + "</p>"+
-                          // <span> الثلاثاء 10/10/2017 - 8.00 مساءا </span>
-                      "</div>"+
-                  "</a>";
-          });
+      // axios.get("http://www.tantasc.net/wp-json/wp/v2/posts?_embed&per_page=10" + param).then(function(response) {
+      axios.get("test/data.json" + param).then(function(response) {
+        var posts = response.data;
+
+        if(sessionStorage.page == 1){
+          var d = new Date(firstPost.date);
+        }
+
+        posts.forEach(post => {
+          var date = moment(post.date);
+
+          var a = date.format('a') == 'am' ? 'صباحا' : 'مساءا';
+
+          document.getElementById("posts").innerHTML += "<a href='post.html#" + post.id + "' class='card mb-4'>" +
+            "<img class='card-img-top' src='" + post._embedded['wp:featuredmedia'][0].source_url + "' height='150px' alt='Card image cap' >" +
+            "<div class='card-body'>" +
+              "<h4 class='card-title news-title'>" + post.title.rendered + "</h4>" +
+              "<p class='card-text news-sub-title'>" +
+              "<span>" + date.locale('ar').format('dddd ') + date.locale('en').format('YYYY/M/D - h:mm ') + a + "</span><br />" +
+              "</p>" +
+              "<i class='mdi mdi-share'></i>" +
+            "</div>" +
+          "</a>";
+        });
+
         }).then(function(){
           navigator.splashscreen.hide();
         });
