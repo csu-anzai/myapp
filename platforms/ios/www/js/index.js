56b525e1380cfd4fd980d90b10f33a0d6ccf2f69
Ahmed-Shaheen <e.shaheen13@gmail.com>
2019-04-17 10:21:40 +0200

    make location open in google maps application

--- platforms/ios/www/js/index.js
+++ platforms/ios/www/js/index.js
@@ -6,32 +6,32 @@ window.addEventListener("message", function(e){
 // Add to index.js or the first page that loads with your app.
 // For Intel XDK and please add this to your app.js.
 
-//document.addEventListener('deviceready', function () {
-//  // Enable to debug issues.
-//  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-//
-//
-//  var notificationOpenedCallback = function(jsonData) {
-//    var data = jsonData;
-//    window.location = 'post.html#' + data.notification.payload.additionalData.post_id;
-//  };
-//
-//  window.plugins.OneSignal
-//    .startInit("ae305c36-5d9d-4e34-8154-924268faea8c")
-//    .handleNotificationOpened(notificationOpenedCallback)
-//    .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.None)
-//    .endInit();
-//}, false);
-//
-//
-//const ptr = PullToRefresh.init({
-//  shouldPullToRefresh: () => !window.scrollY,
-//  mainElement: '#body',
-//  triggerElement: '#body',
-//  onRefresh() {
-//    window.location.reload();
-//  },
-//});
+// document.addEventListener('deviceready', function () {
+//   // Enable to debug issues.
+//   // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+
+
+//   var notificationOpenedCallback = function(jsonData) {
+//     var data = jsonData;
+//     window.location = 'post.html#' + data.notification.payload.additionalData.post_id;
+//   };
+
+//   window.plugins.OneSignal
+//     .startInit("ae305c36-5d9d-4e34-8154-924268faea8c")
+//     .handleNotificationOpened(notificationOpenedCallback)
+//     .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.None)
+//     .endInit();
+// }, false);
+
+
+const ptr = PullToRefresh.init({
+  shouldPullToRefresh: () => !window.scrollY,
+  mainElement: '#body',
+  triggerElement: '#body',
+  onRefresh() {
+    window.location.reload();
+  },
+});
 
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
