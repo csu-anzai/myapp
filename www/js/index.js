fd824000362d61891be4db81ed9fb2ad20072a83
Ahmed-Shaheen <e.shaheen13@gmail.com>
2019-09-03 16:50:59 +0200

    activate push notifications

--- www/js/index.js
+++ www/js/index.js
@@ -6,22 +6,22 @@ window.addEventListener("message", function(e){
 // Add to index.js or the first page that loads with your app.
 // For Intel XDK and please add this to your app.js.
 
-// document.addEventListener('deviceready', function () {
-//   // Enable to debug issues.
-//   // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-//
-//
-//   var notificationOpenedCallback = function(jsonData) {
-//     var data = jsonData;
-//     window.location = 'post.html#' + data.notification.payload.additionalData.post_id;
-//   };
-//
-//   window.plugins.OneSignal
-//     .startInit("ae305c36-5d9d-4e34-8154-924268faea8c")
-//     .handleNotificationOpened(notificationOpenedCallback)
-//     .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.None)
-//     .endInit();
-// }, false);
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+
+
+  var notificationOpenedCallback = function(jsonData) {
+    var data = jsonData;
+    window.location = 'post.html#' + data.notification.payload.additionalData.post_id;
+  };
+
+  window.plugins.OneSignal
+    .startInit("ae305c36-5d9d-4e34-8154-924268faea8c")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.None)
+    .endInit();
+}, false);
 
 
 const ptr = PullToRefresh.init({
